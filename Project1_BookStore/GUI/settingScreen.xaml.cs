511da1734c24af36e26976722ee3e9b8af753867
Nguyen Duc Huy <duchuy040421@gmail.com>
2022-04-08 11:57:42 +0700

    design-setting-screen-v1.0
    finish design setting screen

---
+++ Project1_BookStore/GUI/settingScreen.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project1_BookStore.GUI
+{
+    /// <summary>
+    /// Interaction logic for settingScreen.xaml
+    /// </summary>
+    public partial class settingScreen : Window
+    {
+        public settingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void maxButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Normal:
+                    this.WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    this.WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+
+        private void minButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        Icons _icons = new Icons();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = _icons;
+            //this.WindowState = WindowState.Maximized;
+        }
+    }
+}
