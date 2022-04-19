acaea50380ab6a0637d507cc852591b4584fc9a3
baotram11 <19127588@student.hcmus.edu.vn>
2022-04-19 07:30:51 +0700

    export pdf for order details v1.1

--- Project1_BookStore/GUI/DetailOrderScreen.xaml.cs
+++ Project1_BookStore/GUI/DetailOrderScreen.xaml.cs
@@ -1,7 +1,11 @@
-﻿using Project1_BookStore.BUS;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
+using Project1_BookStore.BUS;
 using Project1_BookStore.DTO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +26,7 @@ namespace Project1_BookStore.GUI
     public partial class DetailOrderScreen : Window
     {
         public OrderDTO _detail { get; set; }
+
         public DetailOrderScreen()
         {
             InitializeComponent();
@@ -56,13 +61,20 @@ namespace Project1_BookStore.GUI
         }
 
         Icons _icons = new Icons();
+        List<OrderDetailDTO> _odsList;
+        CustomerDTO _customer;
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             idOrder.DataContext = _detail;
             total.DataContext = _detail;
             id.DataContext = _detail;
-            customer.DataContext = CustomerBUS.findCustomerByID(_detail.cusPhoneNumber);
+
+            _customer = CustomerBUS.findCustomerByID(_detail.cusPhoneNumber);
+            customer.DataContext = _customer;
+
             infoOrderBasic.DataContext = _detail;
+
             GridContent.ItemsSource = OrderDetailBUS.findOrderDetailByOrderID(_detail.ordersID);
 
             this.DataContext = _icons;
@@ -82,5 +94,180 @@ namespace Project1_BookStore.GUI
             {
             }
         }
+
+        private void exportData_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Tệp PDF (*.pdf)|*.pdf";
+            save.FileName = $"Đơn hàng {_detail.ordersID}";
+
+            bool errMessage = false;
+            if (save.ShowDialog() == true)
+            {
+                if (File.Exists(save.FileName))
+                {
+                    try
+                    {
+                        File.Delete(save.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        errMessage = true;
+                        MessageBox.Show("Không thể lưu tập tin vào ổ đĩa " + ex.Message);
+                    }
+                }
+                else
+                {
+                    //do nothing
+                }
+                if (!errMessage)
+                {
+                    try
+                    {
+                        //define base font for PDF document
+                        string vifontPath = Environment.GetEnvironmentVariable("SystemRoot") + "\\fonts\\times.ttf";
+                        BaseFont viFont = BaseFont.CreateFont(vifontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                        
+                        Font title = new Font(viFont, 25f, Font.BOLDITALIC, BaseColor.BLUE);
+                        Font header = new Font(viFont, 12f, Font.BOLD, BaseColor.BLACK);
+                        Font normal = new Font(viFont, 10f, Font.NORMAL, BaseColor.BLACK);
+                        Font underline = new Font(viFont, 10f, Font.UNDERLINE, BaseColor.BLACK);
+                        
+                        //creating iTextSharp Table from the DataTable data
+                        PdfPTable pdfPTable = new PdfPTable(6);
+                        int[] intTblWidth = { 7, 25, 8, 20, 20, 20 };
+                        
+                        pdfPTable.SetWidths(intTblWidth);
+                        pdfPTable.WidthPercentage = 100;
+                        pdfPTable.DefaultCell.BorderWidth = 1;
+                        pdfPTable.HorizontalAlignment = Element.ALIGN_CENTER;
+                        
+                        //adding header columns
+                        PdfPCell pdfPCell = new PdfPCell(new Phrase("STT", header));
+                        pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell);
+                        
+                        PdfPCell pdfPCell2 = new PdfPCell(new Phrase("Sản phẩm", header));
+                        pdfPCell2.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell2);
+                        
+                        PdfPCell pdfPCell3 = new PdfPCell(new Phrase("Số lượng", header));
+                        pdfPCell3.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell3);
+                        
+                        PdfPCell pdfPCell4 = new PdfPCell(new Phrase("Giá tiền", header));
+                        pdfPCell4.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell4);
+                        
+                        PdfPCell pdfPCell5 = new PdfPCell(new Phrase("Giảm giá", header));
+                        pdfPCell5.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell5);
+                        
+                        PdfPCell pdfPCell6 = new PdfPCell(new Phrase("Tạm tính", header));
+                        pdfPCell6.HorizontalAlignment = Element.ALIGN_CENTER;
+                        pdfPTable.AddCell(pdfPCell6);
+
+
+                        //adding all rows
+                        int i = 1;
+                        foreach (var item in GridContent.Items)
+                        {
+                            OrderDetailDTO order = (OrderDetailDTO)item;
+
+                            PdfPCell pdfPCell7 = new PdfPCell(new Phrase((i++).ToString(), normal));
+                            pdfPCell7.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell7);
+
+                            PdfPCell pdfPCell8 = new PdfPCell(new Phrase(order.bookID, normal));
+                            pdfPCell8.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell8);
+
+                            PdfPCell pdfPCell9 = new PdfPCell(new Phrase(order.odQuantity.ToString(), normal));
+                            pdfPCell9.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell9);
+
+                            PdfPCell pdfPCell10 = new PdfPCell(new Phrase(order.odCurrentPrice.ToString(), normal));
+                            pdfPCell10.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell10);
+
+                            PdfPCell pdfPCell11 = new PdfPCell(new Phrase("0 đ", normal));
+                            pdfPCell11.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell11);
+
+                            PdfPCell pdfPCell12 = new PdfPCell(new Phrase(order.odCurrentPrice.ToString(), normal));
+                            pdfPCell12.HorizontalAlignment = Element.ALIGN_CENTER;
+                            pdfPTable.AddCell(pdfPCell12);
+                        }
+                        
+                        //Exporting to PDF
+                        using (FileStream fileStream = new FileStream(save.FileName, FileMode.Create))
+                        {
+                            Document document = new Document(PageSize.A4, 10f, 10f, 10f, 10f);
+                            PdfWriter.GetInstance(document, fileStream);
+                            document.Open();
+                            document.AddHeader("Title", $"Chi tiết đơn hàng {_detail.ordersID}");
+                            document.AddLanguage("vi-VN");
+                            document.AddTitle($"Đơn hàng {_detail.ordersID}");
+                            document.AddCreationDate();
+
+                            // write Title
+                            document.Add(Chunk.TABBING);
+                            document.Add(Chunk.TABBING);
+                            document.Add(Chunk.TABBING);
+                            document.Add(Chunk.TABBING);
+
+                            document.Add(new Phrase($"Chi tiết đơn hàng {_detail.ordersID}", title));
+                            document.Add(Chunk.NEWLINE);
+                            document.Add(Chunk.NEWLINE); 
+
+                            // write Customer Info
+                            document.Add(new Phrase($"Khách hàng:    {_customer.cusName}", normal));
+                            document.Add(Chunk.NEWLINE);
+                            document.Add(new Phrase($"Số điện thoại: {_customer.cusPhoneNumber}", normal));
+                            document.Add(Chunk.NEWLINE);
+
+                            document.Add(new Phrase($"Ngày mua hàng: {_detail.ordersTime}", normal));
+                            document.Add(Chunk.NEWLINE);
+                            document.Add(new Phrase($"Nhân viên:     {_detail.accUsername}", normal));
+                            document.Add(Chunk.NEWLINE);
+                            document.Add(Chunk.NEWLINE);
+
+                            document.Add(new Phrase($"Chi tiết sản phẩm" , header));
+                            document.Add(Chunk.NEWLINE);
+
+
+                            // write Order Detail
+                            document.Add(pdfPTable);
+
+                            // write Authour
+                            document.AddAuthor($"@{_detail.accUsername}");
+                            document.AddCreator("@username");
+
+                            // write Created Date
+                            document.AddCreationDate();
+
+                            // write number page
+
+                            document.Close();
+                            fileStream.Close();
+                        }
+                        MessageBox.Show("Xuất dữ liệu thành công!",
+                                        "Xuất dữ liệu",
+                                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi xuất dữ liệu!",
+                                        "Xuất dữ liệu",
+                                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+
     }
 }
